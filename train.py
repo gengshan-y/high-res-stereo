66d0de80351c68ea918ba9af70fe34c6c3ab58bf
bzporter <70294041+bzporter@users.noreply.github.com>
2020-10-08 15:57:05 -0600

    Update lidar dataloader (#5)
    flip disparity and set the occlusion size to a smaller min-max

--- train.py
+++ train.py
@@ -98,7 +98,9 @@ def init_dataloader(input_args):
     loader_eth3d = DA.myImageFloder(all_left_img, all_right_img, all_left_disp, rand_scale=rand_scale, order=0)
 
     all_left_img, all_right_img, all_left_disp, all_right_disp = lidar_dataloader('%s/lidar-hdsm-dataset/' %input_args.database)
-    loader_lidar = DA.myImageFloder(all_left_img, all_right_img, all_left_disp, right_disparity=all_right_disp, rand_scale=[0.5, 1.1*scale_factor], order=2)
+    loader_lidar = DA.myImageFloder(all_left_img, all_right_img, all_left_disp, right_disparity=all_right_disp,
+                                    rand_scale=[0.5, 1.1*scale_factor], order=2, flip_disp_ud=True,
+                                    occlusion_size=[10, 25])
 
     data_inuse = torch.utils.data.ConcatDataset([loader_carla] * 10 +
                                                 [loader_mb] * 150 + # 71 pairs
